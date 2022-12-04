e9cdad171ce2414f135aebbac702a3e17b8d32e7
Chris McDevitt <chris.mcdevitt@fundingcircle.com>
2022-12-04 20:12:14 +0000

    day 3 part 2

--- day3.hs
+++ day3.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import Data.Char
+import Data.List.Split (chunksOf)
 import qualified Data.Set as Set
-import Debug.Trace
 
 input =
   "vJrwpWtwJgWrhcsFMMfFFhFp\n\
@@ -16,9 +16,14 @@ main :: IO ()
 main = do
   input <- readFile "input/day3.txt"
   let prios = map winner $ lines input
+      badges = map badge $ chunksOf 3 $ lines input
   print $ sum prios
+  print $ sum $ map (charPrio . Set.elemAt 0) badges
   where
     winner line =
       let (a, b) = splitAt (length line `div` 2) line
-          both = Set.elemAt 0 $ Set.intersection (Set.fromList a) (Set.fromList b)
-       in if isLower both then fromEnum both - 96 else fromEnum both - 38
+       in charPrio $ Set.elemAt 0 $ Set.intersection (Set.fromList a) (Set.fromList b)
+    badge triple =
+      let (a : b : c : _) = map Set.fromList triple
+       in Set.intersection (Set.intersection a b) c
+    charPrio c = if isLower c then fromEnum c - 96 else fromEnum c - 38
