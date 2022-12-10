a0f014c7775da9561ef223b33bdb295dc2c9d8c6
Chris McDevitt <chris.mcdevitt@fundingcircle.com>
2022-12-10 23:44:14 +0000

    day 10 part 2

--- day10.hs
+++ day10.hs
@@ -1,11 +1,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Data.List.Split (splitOn)
-import Data.Map (Map ())
-import Data.Map.Strict ((!))
-import qualified Data.Map.Strict as Map
-import Data.Maybe (fromJust)
-import qualified Data.Set as Set
+import Data.List.Split (chunksOf, splitOn)
 import Debug.Trace (traceShow)
 
 data Op
@@ -13,33 +8,49 @@ data Op
   | NoOp
   deriving (Show, Eq)
 
-cycles :: Set.Set Int
-cycles = Set.fromList [20, 60, 100, 140, 180, 220]
-
 parseLine :: String -> Op
-parseLine ('n' : 'o' : 'o' : 'p' : _) = NoOp
+parseLine ('n' : _) = NoOp
 parseLine ('a' : 'd' : 'd' : 'x' : ' ' : num) = Addx (read num :: Int)
 
 runOp :: (Int, Int, Int) -> Op -> (Int, Int, Int)
-runOp (cycle, x, sig) NoOp = (cycle + 1, x, sig + calcSignal (cycle, x, sig))
+runOp (cycle, x, sig) NoOp = (cycle + 1, x, sig + calcSignal cycle x)
 runOp (cycle, x, sig) (Addx n) =
-  let newsig = calcSignal (cycle, x, sig) + calcSignal (cycle + 1, x, sig)
+  let newsig = calcSignal cycle x + calcSignal (cycle + 1) x
    in (cycle + 2, x + n, newsig + sig)
 
-calcSignal :: (Int, Int, Int) -> Int
-calcSignal (cycle, x, sig) =
-  if Set.member cycle cycles
+calcSignal :: Int -> Int -> Int
+calcSignal cycle x =
+  if cycle `elem` [20, 60, 100, 140, 180, 220]
     then cycle * x
     else 0
 
-input =
-  "noop\n\
-  \addx 3\n\
-  \addx -5"
+drawPixel :: Int -> Int -> Char
+drawPixel cycle x
+  | cycle == x || cycle == x - 1 || cycle == x + 1 = '#'
+  | otherwise = '.'
+
+incCycle :: Int -> Int
+incCycle c = if c == 39 then 0 else c + 1
+
+rendrOp :: (Int, Int, String) -> Op -> (Int, Int, String)
+rendrOp (cycle, x, screen) NoOp = (incCycle cycle, x, screen ++ [drawPixel cycle x])
+rendrOp (cycle, x, screen) (Addx n) =
+  let (c, scr) = (incCycle cycle, screen ++ [drawPixel cycle x])
+   in (incCycle c, x + n, scr ++ [drawPixel c x])
 
 main :: IO ()
 main = do
   input <- readFile "input/day10.txt"
   -- input <- readFile "input/sample10.txt"
   let parsedLines = map parseLine $ lines input
+      (_, _, screenline) = foldl rendrOp (0, 1, "") $ parsedLines
   print $ foldl runOp (1, 1, 0) parsedLines
+  putStr . unlines $ chunksOf 40 screenline
+
+-- 13220
+-- ###..#..#..##..#..#.#..#.###..####.#..#.
+-- #..#.#..#.#..#.#.#..#..#.#..#.#....#.#..
+-- #..#.#..#.#..#.##...####.###..###..##...
+-- ###..#..#.####.#.#..#..#.#..#.#....#.#..
+-- #.#..#..#.#..#.#.#..#..#.#..#.#....#.#..
+-- #..#..##..#..#.#..#.#..#.###..####.#..#.
